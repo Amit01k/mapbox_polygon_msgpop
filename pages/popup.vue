08e7a7abb00331b148885c40767fba21a53563a0
Amit01 <amitkumar1437786@gmail.com>
2022-09-30 10:01:43 +0530

    feat: all working(new)

--- pages/popup.vue
+++ pages/popup.vue
@@ -2,27 +2,30 @@
   <div>
     <div class="flex">
       <!-- <div class="form-class"> -->
-      <form v-if="data1.flag" v-on:submit.prevent class="form-class">
+      <form v-if="state.flag" v-on:submit.prevent class="form-class">
         <tr>
-          <td>Name:<input type="text" id="name" /></td>
+          <td>
+            Name:<input type="text" id="name" v-model="states.obj.name" />
+          </td>
         </tr>
         <br />
-        <tr>
+        <!-- <tr>
           <td>description: <input type="text" /></td>
-        </tr>
-        <button type="submit">submit</button>
+        </tr> -->
+        <button type="submit" @click="submitForm">submit</button>
       </form>
       <!-- </div> -->
       <main class="w-screen h-screen">
         <v-map
           class="w-full h-full"
-          :options="data1.options"
+          :options="state.options"
           @loaded="onMapLoaded"
         />
         <div id="calculated-area"></div>
       </main>
       <div class="dislpay-arena">
-        <div>{{ data1.data2 }}</div>
+        <!-- <div>hiii{{ state.data2 }}</div> -->
+        <input type="checkbox" />{{ state.data2 }}
       </div>
     </div>
   </div>
@@ -34,7 +37,7 @@ import { ref } from "vue";
 import mapboxgl from "mapbox-gl";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
 const name = ref("");
-const data1 = reactive({
+const state = reactive({
   options: {
     accessToken:
       "pk.eyJ1IjoicGF1bDAyIiwiYSI6ImNsNmdnY2p0MTE3MTUzY24yd3d3djZ0cnYifQ.IwKuQR_eGwyojW3tckCTVQ",
@@ -46,6 +49,22 @@ const data1 = reactive({
   flag: false,
 });
 
+const states: any = {
+  //   name: [],
+  obj: {
+    name: "",
+  },
+};
+
+console.log("this is come from line nu-58 ", states.name);
+//console.log("for properties", state.data2.features.geometry[0].properties);
+
+function submitForm() {
+  //   state.data2.push(states.name);
+  state.data2.push(states.obj);
+  console.log("submit date" + state.data2);
+}
+
 // var coordinates = [-77.03171253204346, 38.91457788952212];
 // var description = "<button onclick=' + { this.handlePop } + '> Button</button>"
 function onMapLoaded(map: mapboxgl.Map) {
@@ -56,53 +75,6 @@ function onMapLoaded(map: mapboxgl.Map) {
       trash: true,
       line_string: true,
     },
-    styles: [
-      //linestring style
-      {
-        id: "gl-draw-line",
-        type: "line",
-        filter: [
-          "all",
-          ["==", "$type", "LineString"],
-          ["!=", "mode", "static"],
-        ],
-        layout: {
-          "line-cap": "round",
-          "line-join": "round",
-        },
-        paint: {
-          "line-color": "#0254A2",
-          "line-dasharray": [0.2, 2],
-          "line-width": 5,
-        },
-      },
-      //polygon stroke style
-      {
-        id: "gl-draw-polygon-stroke-active",
-        type: "line",
-        filter: ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
-        layout: {
-          "line-cap": "round",
-          "line-join": "round",
-        },
-        paint: {
-          "line-color": "#0254A2",
-          "line-dasharray": [0.2, 2],
-          "line-width": 5,
-        },
-      },
-      // polygon fill style
-      {
-        id: "gl-draw-polygon-fill",
-        type: "fill",
-        filter: ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
-        paint: {
-          "fill-color": "#0254A2",
-          "fill-outline-color": "#0254A2",
-          "fill-opacity": 0,
-        },
-      },
-    ],
   });
   map.addControl(Draw, "top-left");
   map.on("draw.create", updateArea);
@@ -115,37 +87,15 @@ function onMapLoaded(map: mapboxgl.Map) {
     map.getCanvas().style.cursor = "";
   });
   function updateArea(e) {
-    data1.flag = true;
+    console.log("thi is come from line nu-88", state.data2);
+
+    state.flag = true;
     console.log("clicked");
     const data = Draw.getAll();
-    data1.data2.push(data);
-    map.on("click", "gl-draw-polygon-fill.cold", function (e) {
-      console.log(e.lngLat);
-      //   var description =
-      //     "<form> Enter name: <input type='text' id='name' /></br> Enter Description:<input type='text' id='description'/><button id='view-full'>Submit</button></form > ";
-      let popupElem = new mapboxgl.Popup({ focusAfterOpen: true })
-        .setLngLat(e.lngLat)
-        .setHTML(form)
-        .addTo(map);
-      popupElem.getElement().addEventListener("submit", (e) => {
-        e.preventDefault();
-        var val = document.getElementById("name");
-        var des = document.getElementById("description");
-        var name_value = val.value;
-        var des_value = des.value;
-        let c = data1.data2[0].features[0].properties;
-        //  c.push(name_value)
-        data1.data2.push(name_value);
-        let d = data1.data2[0].features[0];
-        //data1.data2[0].features[0].properties.push(name_value);
-        console.log("this is come from line nu-126", d);
-        // d.push({properties:})
-        console.log(d);
-        console.log(data1.data2[0].features[0].properties, "from console");
-        console.log("hello");
-      });
-    });
+    state.data2.push(data);
+    // state.flag = false;
   }
+  state.flag = false;
 }
 </script>
 <style>
@@ -165,12 +115,7 @@ body {
 .h-screen {
   height: 100vh;
 }
-/* .h-full {
-     height: 100%;
-   }
-   .w-full {
-     width: 85%;
-   } */
+
 .dislpay-arena {
   background: #fdfafae9;
   box-shadow: 0px -3px 10px rgba(0, 58, 78, 0.1);
@@ -194,7 +139,7 @@ body {
   background-color: aqua;
   width: 300px;
   border-radius: 10px;
-  height: 170px;
+  height: 100px;
   position: absolute;
   z-index: 2;
   padding: 5px;
@@ -214,7 +159,7 @@ button {
   border-block-start-color: tomato;
   height: 20px;
   width: 40px;
-  border: 2px solid pink;
+  /* border: 2px solid pink; */
 }
 input {
   /* pa */
